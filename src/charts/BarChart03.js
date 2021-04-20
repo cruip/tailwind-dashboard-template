30b1b7a95e73f2f37d0dcc26a91839b0c8b0a5bb
pasqualevitiello <pasqualevitiello@gmail.com>
2021-04-20 18:57:12 +0200

    horizontal bar chart

--- src/charts/BarChart03.js
+++ src/charts/BarChart03.js
@@ -5,6 +5,9 @@ import {
 } from 'chart.js';
 import 'chartjs-adapter-moment';
 
+// Import utilities
+import { tailwindConfig } from '../utils/Utils';
+
 Chart.register(BarController, BarElement, LinearScale, CategoryScale, Tooltip, Legend);
 
 function BarChart03({
@@ -14,6 +17,7 @@ function BarChart03({
 }) {
 
   const canvas = useRef(null);
+  const legend = useRef(null);
 
   useEffect(() => {
 
@@ -64,17 +68,71 @@ function BarChart03({
           mode: 'nearest'
         },
         animation: {
-          duration: 200,
+          duration: 500,
         },
         maintainAspectRatio: false,
       },
+      plugins: [{
+        id: 'htmlLegend',
+        afterUpdate(c, args, options) {
+          const ul = legend.current;
+          if (!ul) return;
+          // Remove old legend items
+          while (ul.firstChild) {
+            ul.firstChild.remove();
+          }
+          // Reuse the built-in legendItems generator
+          const items = c.options.plugins.legend.labels.generateLabels(c);
+          items.forEach((item) => {
+            console.log(item);
+            const li = document.createElement('li');
+            li.style.display = 'flex';
+            li.style.justifyContent = 'space-between';
+            li.style.alignItems = 'center';
+            li.style.paddingTop = tailwindConfig().theme.padding[2.5];
+            li.style.paddingBottom = tailwindConfig().theme.padding[2.5];
+            const wrapper = document.createElement('div');
+            wrapper.style.display = 'flex';
+            wrapper.style.alignItems = 'center';
+            const box = document.createElement('div');
+            box.style.width = tailwindConfig().theme.width[3];
+            box.style.height = tailwindConfig().theme.width[3];
+            box.style.borderRadius = tailwindConfig().theme.borderRadius.sm;
+            box.style.marginRight = tailwindConfig().theme.margin[3];
+            box.style.backgroundColor = item.fillStyle;
+            const label = document.createElement('div');
+            const value = document.createElement('div');
+            value.style.fontWeight = tailwindConfig().theme.fontWeight.medium;
+            value.style.marginLeft = tailwindConfig().theme.margin[3];
+            value.style.color = item.text === 'Other' ? tailwindConfig().theme.colors.gray[400] : item.fillStyle;
+            const theValue = c.data.datasets[item.datasetIndex].data.reduce((a, b) => a + b, 0);
+            const valueText = document.createTextNode(`${parseInt(theValue / max * 100)}%`);
+            const labelText = document.createTextNode(item.text);
+            value.appendChild(valueText);
+            label.appendChild(labelText);
+            ul.appendChild(li);
+            li.appendChild(wrapper);
+            li.appendChild(value);
+            wrapper.appendChild(box);
+            wrapper.appendChild(label);
+          });
+        },
+      }],
     });
     return () => chart.destroy();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <canvas ref={canvas} width={width} height={height}></canvas>
+    <div className="flex-grow flex flex-col justify-center">
+      <div>
+        <canvas ref={canvas} width={width} height={height}></canvas>
+      </div>
+      <div className="px-5 pt-2 pb-6">
+        <ul ref={legend} className="text-sm divide-y divide-gray-100"></ul>
+        <ul className="text-sm divide-y divide-gray-100"></ul>
+      </div>
+    </div>
   );
 }
 
