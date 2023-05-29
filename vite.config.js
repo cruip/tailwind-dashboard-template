d4b97f2c43043c99ce4909ea7e8e10960456cd80
VICTOR-CHUKWU <okenwa1993@gmail.com>
2023-05-29 14:32:11 +0100

    fixing bug

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    "process.env": process.env,
   },
   css: {
     postcss,
@@ -24,6 +24,6 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  } 
-})
+    },
+  },
+});
