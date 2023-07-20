446e655303ddc8f410bba54f7c604557b85afa0f
pasqualevitiello <pasqualevitiello@gmail.com>
2023-07-20 08:36:34 +0200

    fix banner z-index

--- src/partials/Banner.jsx
+++ src/partials/Banner.jsx
@@ -13,7 +13,7 @@ function Banner() {
   return (
     <>
     { bannerOpen && (
-      <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-60">
+      <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
         <div className="bg-slate-800 border border-transparent dark:border-slate-700 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
           <div className='text-slate-500 inline-flex'><a className="font-medium hover:underline text-slate-50" href={liteLink} target="_blank" rel="noreferrer">Download<span className="hidden sm:inline"> on GitHub</span></a> <span className="italic px-1.5">or</span> <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/mosaic/" target="_blank" rel="noreferrer">Check Premium Version</a></div>
           <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false) }>
